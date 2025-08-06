086d840f68d71191a201ca9ac249c26524579bfc
SASCHADDD <saschadanu23@gmail.com>
2025-08-06 17:12:26 +0700

    tugas4

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 const express = require('express');
-
 const app = express();
 const port = 3001;
+
+const { router: todoRouter } = require("./routes/todo");
+
+// Middleware
 app.use(express.json());
 
+// View Engine
 app.set('view engine', 'ejs');
 
+// Routes
+app.use("/todos", todoRouter); // API route
+
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index'); // views/index.ejs
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact'); 
+  res.render('contact'); // views/contact.ejs
 });
 
-//midleware
+// Middleware 404
 app.use((req, res) => {
-    res.status(404).send('Page not found 404');
+  res.status(404).send('Page not found 404');
 });
 
+// Start Server
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
